c40c99b60ff7cc7587459d7f8186ef397c2189c7
config95 <76443106+config95@users.noreply.github.com>
2023-08-31 00:01:48 +0300

    Lesson 5 - validation

--- App/Calculator/Calculator.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App;
-
-class Calculator {
-    public static function calculate(int $number1, int $number2, callable $func) {
-        return $func($number1, $number2);
-    }
-}
--- App/index.php
+++ App/index.php
@@ -1,25 +1,40 @@
 <?php
-
 error_reporting(E_ALL);
 ini_set('display_errors',true);
-
 require_once 'bootstrap.php';
+$success = false;
 
-$arrfunc = [
-    function($num1, $num2) {
-    return $num1 + $num2;
-},
-    function($num1, $num2) {
-    return $num1 - $num2;
-},
-    function($num1, $num2) {
-    return $num1 / $num2;
-},
-    function($num1, $num2) {
-    return $num1 * $num2;
-},
-    ];
-
-foreach ($arrfunc as $func) {
-   echo \App\Calculator::calculate(33, 3, $func);
+if (!empty($_POST))
+{
+    try {
+        $user = new \App\User();
+        $user->load($_POST['id']? (int) $_POST['id'] : 0);
+        $user->save($_POST);
+    } catch (\Exception $e) {
+        echo $e->getMessage();
+    }
 }
+?>
+
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Validation</title>
+</head>
+<body>
+<form action="/" method="POST">
+    <input type="hidden" name="id" value="<?= $_GET['id']?? 0?>">
+    <label for="name">Name</label>
+    <input type="text" name="name" id="name">
+    <label for="email">Email</label>
+    <input type="email" name="email" id="email">
+    <label for="age">Age</label>
+    <input type="text" name="age" id="age">
+    <button type="submit">Submit</button>
+</form>
+</body>
+</html>
