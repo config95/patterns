224bcebb0dd784505a32e3cbb1c6f6100b110f23
config95 <76443106+config95@users.noreply.github.com>
2023-09-02 21:29:16 +0300

    Lesson 6 - Factory Method

---
+++ App/Patterns/FactoryMethod.php
@@ -0,0 +1,57 @@
+<?php
+interface Car {
+    public function drive(): void;
+}
+
+class Sedan implements Car {
+    public function drive(): void
+    {
+        echo 'Седан двигается по шоссе.';
+    }
+}
+
+class Crossover implements Car {
+    public function drive(): void
+    {
+        echo 'Кроссовер двигается по шоссе.';
+    }
+}
+
+class Truck implements Car {
+    public function drive(): void
+    {
+        echo 'Грузовик двигается по шоссе.';
+    }
+}
+
+interface CarFactory {
+    public static function createCar(): Car;
+}
+
+class SedanFactory implements CarFactory {
+    public static function createCar(): Car
+    {
+        return new Sedan();
+    }
+}
+
+class CrossoverFactory implements CarFactory {
+    public static function createCar(): Car
+    {
+        return new Crossover();
+    }
+}
+
+class TruckFactory implements CarFactory {
+    public static function createCar(): Car
+    {
+        return new Truck();
+    }
+}
+
+$sedan = SedanFactory::createCar();
+$crossover = CrossoverFactory::createCar();
+$truck = TruckFactory::createCar();
+$sedan->drive();
+$crossover->drive();
+$truck->drive();
