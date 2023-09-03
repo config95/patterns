c86f63a056101814aead2b228670d50e42c8be17
config95 <76443106+config95@users.noreply.github.com>
2023-09-03 22:54:14 +0300

    Lesson 6 - Adapter

---
+++ App/Patterns/Adapter.php
@@ -0,0 +1,24 @@
+<?php
+class ThirdPartyPaymentGateway {
+    public function processPayment(float $amount, string $account): bool {
+        // Обработка платежа через стороннюю систему
+        return true;
+    }
+}
+interface PaymentProcessor {
+    public function charge(float $amount, string $customer): bool;
+}
+
+class PaymentProcessorAdapter implements PaymentProcessor {
+    private $adaptee;
+
+    public function __construct()
+    {
+        $this->adaptee = new ThirdPartyPaymentGateway();
+    }
+
+    public function charge(float $amount, string $customer): bool
+    {
+        return $this->adaptee->processPayment($amount, $customer);
+    }
+}
