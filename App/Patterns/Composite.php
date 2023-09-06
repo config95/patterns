ae43e412f72f2dab2c7faf18b06440e7e729dc0d
config95 <76443106+config95@users.noreply.github.com>
2023-09-07 01:33:11 +0300

    Lesson 6 - Composite

---
+++ App/Patterns/Composite.php
@@ -0,0 +1,69 @@
+<?php
+
+interface Structure {
+    public function create(): string;
+}
+
+
+class Directory implements Structure {
+    protected array $list = [];
+    protected string $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function addElement(Structure $element) {
+        $this->list[] = $element;
+        return $this;
+    }
+
+    public function create(): string
+    {
+        $str = 'В папке ' . $this->name . '<br>';
+
+        foreach ($this->list as $item) {
+            $str .= $item->create() . '<br>';
+        }
+
+        return $str;
+    }
+}
+
+class File implements Structure {
+    protected string $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+    public function create(): string
+    {
+        return $this->name;
+    }
+}
+
+
+$departments = new Directory('departments');
+$it = new Directory('IT');
+$hr = new Directory('HR');
+
+$ivanov = new File('Иванов.txt');
+$petrov = new File('Петров.txt');
+$sidorov = new File('Сидоров.txt');
+$kireev = new File('Киреев.txt');
+
+$structure = $departments
+    ->addElement(
+        $it
+            ->addElement($ivanov)
+            ->addElement($petrov)
+    )
+    ->addElement(
+        $hr
+            ->addElement($sidorov)
+            ->addElement($kireev)
+    );
+
+echo $structure->create();
